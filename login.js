718f8bc2ebea9676fee6c32f204ef6078a9ea176
y4mch1 <143470122+y4mch1@users.noreply.github.com>
2023-10-14 11:30:12 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+const showHide = document.getElementById('show-hide');
+let passwordInput = document.getElementById('password');
+
+showHide.addEventListener('click', function () {
+    showHide.classList.toggle('show');
+
+    if (showHide.classList.contains('show')) {
+        showHide.classList.remove('fa-eye-slash');
+        showHide.classList.add('fa-eye');
+        passwordInput.setAttribute('type', 'text');
+    }
+    else {
+        showHide.classList.add('fa-eye-slash');
+        showHide.classList.remove('fa-eye');
+        passwordInput.setAttribute('type', 'password');
+    }
+});
+
+
+const loginForm = document.querySelector('form');
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+
+   
+    window.location.href = 'index.html';
+});
